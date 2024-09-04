d6fdad2b45086b148e08d58cfac154d31dc09448
Emmaculate <emmamwitimi14@gmail.com>
2024-09-04 14:08:53 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self,list):
+        matches=[]
+        for list_item in list:
+            if sorted(list_item) == self.sorted_word:
+                matches.append(list_item)
+
+        return matches
+
